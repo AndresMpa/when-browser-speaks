fb24359e9746c15c6a35ed18533f99026510c7a1
Andres M. Prieto <andresprieto2703@gmail.com>
2023-08-15 04:54:13 -0500

    Multi language support
    Adding multi language support for es and en

---
+++ 02_proxy/src/main.js
@@ -0,0 +1,14 @@
+import spanishActions from "./actions/spanish.js";
+import englishActions from "./actions/english.js";
+import proxyManager from "./proxy/manager.js";
+import { languageDetector } from "./util/language.js";
+
+const getter = document.querySelector("#getter");
+let actions;
+
+if (languageDetector("es")) {
+  actions = spanishActions(getter);
+} else {
+  actions = englishActions(getter);
+}
+getter.addEventListener("keyup", proxyManager(getter, actions), false);
