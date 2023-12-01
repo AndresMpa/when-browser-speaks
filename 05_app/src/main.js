7598ce8d9fba80af6a6b7e25a59a33ac6b2630d3
Andres M. Prieto <andresprieto2703@gmail.com>
2023-11-30 21:04:14 -0500

    Just some boiler plate

---
+++ 05_app/src/main.js
@@ -0,0 +1,8 @@
+const transcription = document.querySelector(".transcription");
+const description = document.querySelector(".description");
+const recorder = document.querySelector(".recorder");
+const about = document.querySelector(".about");
+
+const switchMatrix = () => {
+  const matrix = [recorder, transcription, description, about];
+};
